a502d976c8740fd80cc887a62aa6c1664a009c64
Kyle Tunis <kyle.h.tunis@gmail.com>
2025-11-05 11:47:39 +0100

    significant cleanup

--- assignment_1.py
+++ assignment_1.py
@@ -5,7 +5,6 @@ np.random.seed(40)
 SAMPLE_SIZE = 100000
 BOX_BOUND = 2.5
 
-
 def is_in_sphere(x, y, z, k):
     """
     Conditional function that checks if a point (x,y,z) is within a sphere with radius k.
@@ -43,15 +42,20 @@ def deterministic_sample_single_sequence(num_samples, x_0, m):
     return samples
 
 
-def deterministic_sample(num_samples, m=3.8):
-    return (
-        deterministic_sample_single_sequence(num_samples, 0.3, m),
-        deterministic_sample_single_sequence(num_samples, 0.5, m),
-        deterministic_sample_single_sequence(num_samples, 0.7, m)
-    )
+def deterministic_sample(num_samples, box, m=3.8, seed=0.5):
+    sample_seeds = deterministic_sample_single_sequence(3, seed, m)
+    x = deterministic_sample_single_sequence(num_samples, sample_seeds[0], m)
+    y = deterministic_sample_single_sequence(num_samples, sample_seeds[1], m)
+    z = deterministic_sample_single_sequence(num_samples, sample_seeds[2], m)
+
+    x = x * BOX_BOUND - BOX_BOUND/2
+    y = y * BOX_BOUND - BOX_BOUND/2
+    z = z * BOX_BOUND - BOX_BOUND/2
+    return (x, y, z)
 
 
-def points_in_intersection(x_array, y_array, z_array, k, r, R, x_0, y_0, z_0):
+def points_in_intersection(samples, k, r, R, x_0=0, y_0=0, z_0=0):
+    x_array, y_array, z_array = samples
     """
     Checks for points (x,y,z) if they exist in the intersection between a sphere with radius
     k and a torus with minor radius r, major radius R, optionally off-center.
@@ -59,10 +63,12 @@ def points_in_intersection(x_array, y_array, z_array, k, r, R, x_0, y_0, z_0):
     Returns number of points in intersection, and the x, y and z arrays containing those points.
     """
     mask = is_in_sphere(x_array, y_array, z_array, k) & is_in_torus(x_array, y_array, z_array, r, R, x_0, y_0, z_0)
-    return mask.sum(), x_array[mask], y_array[mask], z_array[mask]
+    intersection = (x_array[mask], y_array[mask], z_array[mask])
+    non_intersection = (x_array[~mask], y_array[~mask], z_array[~mask])
+    return intersection, non_intersection
 
 
-def plot_intersection(x, y, z, sample_fraction=1):
+def plot_intersection(x, y, z, sample_fraction=1, fig=None):
     """
     Displays points in 3D space using a scatter plot. If sample_fraction is passed and not 1,
     the defined fraction will be sampled from the points for plotting.
@@ -70,47 +76,51 @@ def plot_intersection(x, y, z, sample_fraction=1):
     if sample_fraction != 1:
         idx = np.random.choice(len(x), int(len(x) * sample_fraction), replace=False)
         x, y, z = x[idx], y[idx], z[idx]
-    fig = plt.figure()
+    if fig is None:
+        fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     ax.scatter(x, y, z)
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$y$')
     ax.set_zlabel(r'$z$')
-    plt.show()
 
 
-def estimate_intersection_volume(x, y, z, k, r, R, box_bound, x_0=0, y_0=0, z_0=0):
+def estimate_volume(intersection, box_bound):
+    x_in, _, _ = intersection
     """
     Estimates the volume of the intersection between a sphere with radius k and a torus with
     minor radius r, major radius R using Monte Carlo simulation and random uniform sampling.
     """
-    in_intersection, x_in, y_in, z_in = points_in_intersection(x, y, z, k, r, R, x_0, y_0, z_0)
     in_box = SAMPLE_SIZE
-    estimated_volume = (in_intersection / in_box) * box_bound**3
-
-    plot_intersection(x_in, y_in, z_in, sample_fraction=0.1)
+    estimated_volume = (len(x_in) / in_box) * box_bound**3
     return estimated_volume
 
 
-def estimate_intersection_non_deterministic(k, r, R, box_bound, x_0=0, y_0=0, z_0=0):
-    x, y, z = uniform_random_sample_cube(box_bound, SAMPLE_SIZE)
-    return estimate_intersection_volume(x, y, z, k, r, R, box_bound, x_0, y_0, z_0)
-
-
-def estimate_intersection_deterministic(k, r, R, box_bound, x_0=0, y_0=0, z_0=0):
-    x, y, z = deterministic_sample(SAMPLE_SIZE)
-    return estimate_intersection_volume(x, y, z, k, r, R, box_bound, x_0, y_0, z_0)
-
-
 print("ASSIGNMENT 1.1")
-print(f"Case a: k = 1, R = 0.75 and r = 0.4, ESTIMATED AREA: {estimate_intersection_non_deterministic(1, 0.4, 0.75, BOX_BOUND)}")
-print(f"Case b: k = 1, R = 0.5 and r = 0.5, ESTIMATED AREA: {estimate_intersection_non_deterministic(1, 0.5, 0.5, BOX_BOUND)}")
+samples = uniform_random_sample_cube(BOX_BOUND, SAMPLE_SIZE)
+intersection, nonintersection = points_in_intersection(samples, 1, 0.4, 0.75)
+volume = estimate_volume(intersection, BOX_BOUND)
+print(f"Case a: k = 1, R = 0.75 and r = 0.4, ESTIMATED AREA: {volume}")
+samples = uniform_random_sample_cube(BOX_BOUND, SAMPLE_SIZE)
+intersection, nonintersection = points_in_intersection(samples, 1, 0.5, 0.5)
+volume = estimate_volume(intersection, BOX_BOUND)
+print(f"Case b: k = 1, R = 0.5 and r = 0.5, ESTIMATED AREA: {volume}")
 
 
 print("ASSIGNMENT 1.2")
-print(f"Case a: k = 1, R = 0.75 and r = 0.4, ESTIMATED AREA: {estimate_intersection_deterministic(1, 0.4, 0.75, BOX_BOUND)}")
-print(f"Case b: k = 1, R = 0.5 and r = 0.5, ESTIMATED AREA: {estimate_intersection_deterministic(1, 0.5, 0.5, BOX_BOUND)}")
+samples = deterministic_sample(SAMPLE_SIZE, BOX_BOUND, seed=0.1)
+intersection, nonintersection = points_in_intersection(samples, 1, 0.4, 0.75)
+volume = estimate_volume(intersection, BOX_BOUND)
+print(f"Case a: k = 1, R = 0.75 and r = 0.4, ESTIMATED AREA: {volume}")
+samples = deterministic_sample(SAMPLE_SIZE, BOX_BOUND, seed=0.1)
+intersection, nonintersection = points_in_intersection(samples, 1, 0.5, 0.5)
+volume = estimate_volume(intersection, BOX_BOUND)
+print(f"Case b: k = 1, R = 0.5 and r = 0.5, ESTIMATED AREA: {volume}")
 
 
 print("ASSIGNMENT 1.3a")
-print(f"Case: k = 1, R = 0.75, r = 0.4 and z_0 = 0.1, ESTIMATED AREA: {estimate_intersection_non_deterministic(1, 0.4, 0.75, BOX_BOUND, z_0=0.1)}")
+samples = uniform_random_sample_cube(BOX_BOUND, SAMPLE_SIZE)
+intersection, nonintersection = points_in_intersection(samples, 1, 0.4, 0.75, z_0=0.1)
+volume = estimate_volume(intersection, BOX_BOUND)
+print(f"Case: k = 1, R = 0.75, r = 0.4 and z_0 = 0.1, ESTIMATED AREA: {volume}")
+
