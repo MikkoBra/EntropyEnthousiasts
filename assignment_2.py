4ff512d8103a8ab0a2028cab271dcb0d4db73f76
Kyle Tunis <kyle.h.tunis@gmail.com>
2025-11-14 10:28:12 +0100

    setup for a2

---
+++ assignment_2.py
@@ -0,0 +1,25 @@
+# Part 1
+
+## Calculate Arrival Rate (λ) in passengers per minute
+
+# Part 2
+
+## Build a discrete-event simulator
+
+## Validate the simulator
+
+### Establish Stable Test Rate
+
+### Theoretical Baseline
+
+### Simulation Baseline
+
+### Hypothesis Test
+
+## Evaluating Intervations
+
+### Intervention A
+
+### Intervention B
+
+### Bonus Intervention
