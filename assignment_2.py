0b25755e53b3e5ed8129d01f8554db4593b8b2a3
MikkoBra <m.brandon.1@student.rug.nl>
2025-11-20 14:43:15 +0100

    CSV data reader function

--- assignment_2.py
+++ assignment_2.py
@@ -1,3 +1,43 @@
+import csv
+import numpy as np
+
+COLUMNS = np.array(["Year", "Month", "Europe Passengers", "Intercontinental Passengers", "Total Passengers"])
+
+
+def extract_columns(columns=COLUMNS, csv_path="airport.csv", delimiter=","):
+    """
+    Extracts column data from a csv file into numpy arrays.
+
+    Parameters
+    ----------
+    columns: Array of column names to extract
+    csv_path: String representing the filepath to the csv file
+    delimiter: String delimiter in the csv file
+    
+    Returns
+    ----------
+    Dictionary containing column names as keys, numpy data arrays as values.
+    """
+    # Extract data to dictionary
+    extracted = {col: [] for col in columns}
+    with open(csv_path, newline="", encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter=delimiter)
+        header = next(reader)
+        col_indices = []
+        for col in columns:
+            if col not in header:
+                raise ValueError(f"Column '{col}' not found in CSV.")
+            col_indices.append(header.index(col))
+        for row in reader:
+            for name, index in zip(COLUMNS, col_indices):
+                extracted[name].append(row[index])
+    
+    # Convert value arrays to numpy arrays
+    for name in extracted:
+        extracted[name] = np.array(extracted[name])
+    
+    return extracted
+
 # Part 1
 
 ## Calculate Arrival Rate (λ) in passengers per minute
